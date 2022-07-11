8b54b55c900dc7101a37e21b7511aa80fef72794
Chinnikrishna275 <108972900+Chinnikrishna275@users.noreply.github.com>
2022-07-11 17:04:33 +0530

    Create DAY1_PRO1.C

---
+++ DAY1/DAY1_PRO1.C
@@ -0,0 +1,9 @@
+.We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+ Print “SPECIAL” if the given non-negative number is special. Use the % "mod" operator.
+#include<stdio.h>
+int main(){
+int n;
+printf("enter the value");
+scanf("%d",&n);
+(n%==0||n%==1) ? printf("spcial"):printf("nothing");
+}

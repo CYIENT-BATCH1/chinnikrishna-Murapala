050f206a8a3b27b37481c06283b08a2174dbe234
Chinnikrishna275 <108972900+Chinnikrishna275@users.noreply.github.com>
2022-07-11 23:48:49 +0530

    Create DAY12_PRO2.C

---
+++ DAY12/DAY12_PRO2.C
@@ -0,0 +1,46 @@
+Find empty strings in an array of strings (Array of strings will be given as input to program) and print the position of array where empty string is present. And replace the empty string array element as “CYIENT”
+
+
+Switch status valueMeaning
+
+
+00Fault type 1 in switch
+
+
+01switch is buckle
+
+
+10switch is unbuckle
+
+
+11Fault type 2 in switch
+
+ Algorithm:
+step1:declear pointer array and some elements.
+step2:find size of that array.
+step by using for loop and if condtion we get pos of that empty string. 
+ #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    int main()
+{
+    char *p[]={"sai","mohan","rahul"," ","raju"};
+    int size=sizeof p/sizeof(p[0]);
+    int pos=1;
+    printf("%d",size);
+    int i,j,k,count=0;
+
+    for(i=0;i<5;i++){
+        printf("\n");
+        if(p[i]==" "){
+            pos=pos+3;
+
+            p[i]="cyient";
+
+        }
+    printf("%s ",*(p+i));
+
+    }
+    printf("\n");
+     printf("pos=%d\n",pos);
+    return 0;  

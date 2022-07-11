f20c19ba112c4165e528d7a599ea8b53841ede79
Chinnikrishna275 <108972900+Chinnikrishna275@users.noreply.github.com>
2022-07-11 23:14:45 +0530

    Create DAY8_PRO1.C

---
+++ DAY8/DAY8_PRO1.C
@@ -0,0 +1,15 @@
+Realize a function which has 2 arguments and 1 return value. Argument 1 is Byte value, Argument 2 is bit position. Function has to flip the bit position (as per argument 2)
+ in argument 1 byte value and return complete byte value.
+Algorithm:
+step1:declear variable a.
+step2:we call function from main function to above function
+step3:we get result .S
+#include<stdio.h>
+    int bitposition(int arg1,int arg2)
+    {
+        arg1=arg1^(1<<arg2);
+    }
+    int main()
+{
+    int a=0x81;
+    printf("%d",(a));

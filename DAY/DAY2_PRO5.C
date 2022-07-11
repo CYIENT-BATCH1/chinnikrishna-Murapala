6e9fb09773effb9e7e9eb9c9a5bb1087f19e11da
Chinnikrishna275 <108972900+Chinnikrishna275@users.noreply.github.com>
2022-07-11 22:57:42 +0530

    Create DAY2_PRO5.C

---
+++ DAY/DAY2_PRO5.C
@@ -0,0 +1,11 @@
+VWrite a program to convert Celsius form of temperature into Fahrenheit and vice versa.
+ #include <stdio.h>
+int main()
+{
+    int n;
+    float y;
+    printf("enter the number value");
+    scanf("%d",&n);
+    y=n*1.8+32;
+    printf("%f",y);
+}
